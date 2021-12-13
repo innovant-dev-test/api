3898b9a859484ae472809fd4590f59dea99cdc50
yessine47 <75212246+yessine47@users.noreply.github.com>
2021-12-13 19:36:51 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const MONGOURI = "mongodb+srv://yessine:yessine147@yessine.km5xi.mongodb.net/innovant?retryWrites=true&w=majority";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
